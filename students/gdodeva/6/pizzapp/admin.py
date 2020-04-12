ce2e0814f6199d18383fd52c04e72e4936951cd3
Gabriela Dodeva <52181534+gdodeva@users.noreply.github.com>
2020-04-12 18:53:08 +0300

    Update admin.py

--- students/gdodeva/6/pizzapp/admin.py
+++ students/gdodeva/6/pizzapp/admin.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals  # noqa: WPS422
 
 from django.contrib import admin
 
-from pizza.models import Ingredient, Order, Pizza
+from pizzapp.models import Ingredient, Order, Pizza
 
 admin.site.register(Ingredient)
 admin.site.register(Pizza)
