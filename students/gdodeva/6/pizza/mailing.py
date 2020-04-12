3d75982cd5d61fc76101ff7090241b591dfbdba0
Gabriela Dodeva <52181534+gdodeva@users.noreply.github.com>
2020-04-12 16:03:01 +0300

    Update mailing.py

--- students/gdodeva/6/pizza/mailing.py
+++ students/gdodeva/6/pizza/mailing.py
@@ -2,14 +2,14 @@
 
 from django.core.mail import send_mail
 
-from pizza.settings import EMAIL_HOST_USER
+from pizzapp.settings import EMAIL_HOST_USER
 
 
-def send_mail_on_order(delivery_time: int, customer_email: str) -> None:
+def send_mail_on_order(delivery_time: int, customer_email: str, customer_address: str) -> None:  # noqa: E501
     """Sends email with order info to customer address."""
     send_mail(
         'Pizza mail',
-        'Your order will be delivered to in {0} min.'.format(delivery_time),
+        'Your order will be delivered to {0} in {1} min.'.format(customer_address, delivery_time),  # noqa: E501
         EMAIL_HOST_USER,
         [customer_email],
     )
